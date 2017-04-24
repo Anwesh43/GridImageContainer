2e5a55bc63903c1b0af2bb7086599a0555192428
Anwesh43 <anweshthecool0@gmail.com>
2017-04-24 13:21:49 +0530

    handling tap and drawing image elements in ImageContainerView

--- gridimagecontainer/src/main/java/com/anwesome/ui/gridimagecontainer/ImageContainerView.java
+++ gridimagecontainer/src/main/java/com/anwesome/ui/gridimagecontainer/ImageContainerView.java
@@ -2,8 +2,11 @@ package com.anwesome.ui.gridimagecontainer;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
+
+import com.anwesome.ui.gridimagecontainer.controller.AnimationController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +16,26 @@ import java.util.List;
  */
 public class ImageContainerView extends View{
     private List<ImageElement> imageElements = new ArrayList<>();
+    private int time = 0;
+    private AnimationController animationController;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     public ImageContainerView(Context context) {
         super(context);
     }
     public void onDraw(Canvas canvas) {
-
+        if(time == 0) {
+            animationController = new AnimationController(this,imageElements);
+        }
+        for(ImageElement imageElement:imageElements) {
+            imageElement.draw(canvas,paint);
+        }
+        time++;
+        animationController.animate();
     }
     public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN && animationController != null) {
+            animationController.handleTap(event.getX(),event.getY());
+        }
         return true;
     }
 }
