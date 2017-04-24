41854e42f9689a206050a0d4b0f52de0fc9fcd48
Anwesh43 <anweshthecool0@gmail.com>
2017-04-24 19:15:18 +0530

    Using GridImageContainer in app

--- app/src/main/java/com/anwesome/ui/gridimagecontainerdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/gridimagecontainerdemo/MainActivity.java
@@ -12,16 +12,22 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private List<Bitmap> bitmaps = new ArrayList<>();
+    GridImageContainer gridImageContainer;
     private int resources[] = {R.drawable.bluetooth,R.drawable.sync,R.drawable.data,R.drawable.wifi};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GridImageContainer gridImageContainer = new GridImageContainer(this);
+        gridImageContainer = new GridImageContainer(this);
         for(int i=0;i<resources.length;i++) {
             bitmaps.add(BitmapFactory.decodeResource(getResources(),resources[i]));
         }
         gridImageContainer.addImages(bitmaps,0,0);
         gridImageContainer.addImages(bitmaps,300,0);
+    }
+    public void onBackPressed() {
+        if(!gridImageContainer.handleShrink()) {
+            super.onBackPressed();
+        }
     }
 }
