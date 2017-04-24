4628ba99f800aa1f84ca03928564794bfd12636f
Anwesh43 <anweshthecool0@gmail.com>
2017-04-24 16:54:13 +0530

    Adding imagecontainer

--- gridimagecontainer/src/main/java/com/anwesome/ui/gridimagecontainer/ImageContainer.java
+++ gridimagecontainer/src/main/java/com/anwesome/ui/gridimagecontainer/ImageContainer.java
@@ -1,7 +1,33 @@
 package com.anwesome.ui.gridimagecontainer;
 
+import android.graphics.Bitmap;
+
+import java.util.List;
+
 /**
  * Created by anweshmishra on 24/04/17.
  */
 public class ImageContainer {
+    private ImageContainerView imageContainerView;
+    private float x,y,scale = 0.2f,initX,initY;
+    public ImageContainer(ImageContainerView imageContainerView,float x,float y) {
+        this.x = x;
+        this.y = y;
+        this.initX = x;
+        this.initY = y;
+        this.imageContainerView = imageContainerView;
+    }
+    public float getScale() {
+        return scale;
+    }
+    public void updateScale(float factor) {
+        this.scale = 0.2f+0.8f*factor;
+    }
+    public void updateXY(float factor) {
+        x = initX*(1-factor);
+        y = initY*(1-factor);
+    }
+    public static ImageContainer create(ImageContainerView imageContainerView, float x, float y) {
+        return new ImageContainer(imageContainerView,x,y);
+    }
 }
